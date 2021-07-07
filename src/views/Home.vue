88b75e7d77476b846c86da3f79c7835b237645e4
h.mori <h.mori@sonicmoov.com>
2021-07-07 18:26:02 +0900

    add eslint-plugin-prettier

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Hello Eslint!!!</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
   },
